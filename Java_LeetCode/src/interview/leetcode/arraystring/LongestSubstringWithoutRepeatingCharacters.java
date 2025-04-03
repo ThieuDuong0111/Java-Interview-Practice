b75195bfedbf25ca0360646f29d2d1cab3589b0e
thieu.duong <duongthieu1995@gmail.com>
2025-04-03 16:54:35 +0700

    add leetcode twosum

---
+++ Java_LeetCode/src/interview/leetcode/arraystring/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,27 @@
+package interview.leetcode.arraystring;
+
+//Bài toán: Longest Substring Without Repeating Characters
+//Mô tả:
+//Cho một chuỗi s, bạn cần tìm độ dài của chuỗi con dài nhất mà không chứa ký tự lặp lại.
+//
+//Ví dụ:
+//Input: "abcabcbb" Output: 3
+//Giải thích: Chuỗi con dài nhất không chứa ký tự lặp lại là "abc", với độ dài là 3.
+//
+//Input: "bbbbb" Output: 1
+//Giải thích: Chuỗi con dài nhất không chứa ký tự lặp lại là "b", với độ dài là 1.
+//
+//Input: "pwwkew" Output: 3
+//Giải thích: Chuỗi con dài nhất không chứa ký tự lặp lại là "wke", với độ dài là 3.
+//
+//Yêu cầu:
+//Viết một hàm có độ phức tạp thời gian O(N) để giải quyết bài toán này.
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
